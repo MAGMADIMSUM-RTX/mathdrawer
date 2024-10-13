090944123b88c8c9f42c1ec3208a51edf73d8a56
MAGMADIMSUM <madmaliu@bupt.edu.cn>
2024-10-13 12:09:08 +0800

    优化硅太阳能电池特性图示
    - 统一了图表中数据点的标记符号，提高了可读性
    - 在功率曲线图中添加了第二个y轴，用于区分和强调单晶硅和非晶硅电池的功率输出
    - 调整了部分图表的坐标轴标签和标题位置，优化了整体布局
    - 设置了图表的底部边界，确保数据的完整性

--- tab.py
+++ tab.py
@@ -2,18 +2,18 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
 plt.rcParams['font.family'] = 'STFangsong'
 
 fig = plt.figure()
 fig.suptitle(r'实验3.18 硅太阳能电池的光照特性', fontsize=18)
-
 
 sub = fig.add_subplot(221)
 df = pd.read_excel('./dat.xlsx', sheet_name='Sheet1')
 line1, = sub.plot(df['V'], df['I'], marker='o', markersize=4,  linewidth=2, linestyle='-',
                   color='red')
 df2 = pd.read_excel('./dat.xlsx', sheet_name='Sheet2')
-line2, = sub.plot(df2['V'], df2['I'], marker='^', markersize=4, linewidth=2,
+line2, = sub.plot(df2['V'], df2['I'], marker='o', markersize=4, linewidth=2,
                   linestyle=':',  color='black')
 sub.legend(handles=[line1, line2], labels=[
            '单晶硅', '非晶硅'], loc='best')
@@ -31,7 +31,7 @@ df = pd.read_excel('./dat.xlsx', sheet_name='Sheet3')
 line1, = sub2.plot(np.log(df['Iin']), df['V'],  marker='o', markersize=4, linewidth=2,
                    linestyle='-',  color='red')
 df2 = pd.read_excel('./dat.xlsx', sheet_name='Sheet4')
-line2, = sub2.plot(np.log(df2['Iin']), df2['V'], marker='^', markersize=4, linewidth=2,
+line2, = sub2.plot(np.log(df2['Iin']), df2['V'], marker='o', markersize=4, linewidth=2,
                    linestyle=':',  color='black')
 sub2.legend(handles=[line1, line2], labels=[
     '单晶硅', '非晶硅'], loc='best')
@@ -41,6 +41,7 @@ sub2.set_xlabel(r'ln($I_{in}$(mA))', fontsize=12, rotation=0, loc='right')
 sub2.set_title(r'硅太阳能电池的$V_{OC}$-ln($I_{in}$)图线', fontsize=16,)
 plt.gca().xaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: f'{x:.1f}'))
 plt.gca().yaxis.set_major_formatter(plt.FuncFormatter(lambda y, _: f'{y:.1f}'))
+# sub2.set_ylim(bottom=2.3)
 
 
 sub3 = fig.add_subplot(223)
@@ -48,7 +49,7 @@ df = pd.read_excel('./dat.xlsx', sheet_name='Sheet3')
 line1, = sub3.plot(df['Iin'], df['Isc'],  marker='o', markersize=4, linewidth=2,
                    linestyle='-',  color='red')
 df2 = pd.read_excel('./dat.xlsx', sheet_name='Sheet4')
-line2, = sub3.plot(df2['Iin'], df2['Isc'], marker='^', markersize=4, linewidth=2,
+line2, = sub3.plot(df2['Iin'], df2['Isc'], marker='o', markersize=4, linewidth=2,
                    linestyle=':',  color='black')
 sub3.legend(handles=[line1, line2], labels=[
     '单晶硅', '非晶硅'], loc='best')
@@ -62,20 +63,33 @@ plt.gca().yaxis.set_major_formatter(plt.FuncFormatter(lambda y, _: f'{y:.1f}'))
 
 sub4 = fig.add_subplot(224)
 df = pd.read_excel('./dat.xlsx', sheet_name='Sheet5')
-line1, = sub4.plot(df['R'], df['I']**2*df['R']/1000,  marker='o', markersize=4, linewidth=2,
-                   linestyle='-',  color='red')
-df2 = pd.read_excel('./dat.xlsx', sheet_name='Sheet6')
-line2, = sub4.plot(df2['R'], df2['I']**2*df['R']/1000, marker='^', markersize=4, linewidth=2,
-                   linestyle=':',  color='black')
-sub4.legend(handles=[line1, line2], labels=[
-    '单晶硅', '非晶硅'], loc='best')
-sub4.grid(which='major')
-sub4.set_ylabel(r'$I^2R$(W)', fontsize=12, rotation=0, loc='top')
-sub4.set_xlabel(r'$R$(Ω)', fontsize=12, rotation=0, loc='right')
-sub4.set_title(r'硅太阳能电池的功率曲线', fontsize=16,)
-plt.gca().xaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: f'{x:.1f}'))
-plt.gca().yaxis.set_major_formatter(plt.FuncFormatter(lambda y, _: f'{y:.1f}'))
+line1, = sub4.plot(df['R'], df['I']**2*df['R']/1000, marker='o', markersize=4, linewidth=2,
+                   linestyle='-', color='red')
 
+sub4_2 = sub4.twinx()
+df2 = pd.read_excel('./dat.xlsx', sheet_name='Sheet6')
+line2, = sub4_2.plot(df2['R'], df2['I']**2*df2['R']/1000, marker='o', markersize=4, linewidth=2,
+                     linestyle=':', color='black')
+
+sub4.legend(handles=[line1, line2], labels=['单晶硅', '非晶硅'], loc='best')
+sub4.grid(which='major')
+# sub4.set_ylabel('$I^2R$(mW)\n单晶硅', fontsize=12, labelpad=-10,
+#                 rotation=0, loc='top', color='black')
+# sub4_2.set_ylabel('$I^2R$(mW)\n非晶硅', fontsize=12, labelpad=40,
+#                   rotation=0, loc='top', color='black')
+sub4.text(-0.05, 1.0, '$I^2R$(mW)\n单晶硅', fontsize=12, ha='center',
+          va='center', transform=sub4.transAxes, color='black')
+sub4_2.text(1.05, 1.0, '$I^2R$(mW)\n非晶硅', fontsize=12, ha='center',
+            va='center', transform=sub4_2.transAxes, color='black')
+
+sub4.set_xlabel(r'$R$(Ω)', fontsize=12, rotation=0, loc='right')
+sub4.set_title(r'硅太阳能电池的功率曲线', fontsize=16)
+
+sub4.xaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: f'{x:.1f}'))
+sub4.yaxis.set_major_formatter(plt.FuncFormatter(lambda y, _: f'{y:.1f}'))
+sub4_2.yaxis.set_major_formatter(plt.FuncFormatter(lambda y, _: f'{y:.1f}'))
+sub4.set_ylim(bottom=0)
+sub4_2.set_ylim(bottom=0)
 
 plt.show()
 plt.close('all')
